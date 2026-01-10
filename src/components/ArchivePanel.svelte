bbadc645d4faacb5e0ab1328f5c131f2d7371755
SkonJIYHD <2661028830@qq.com>
2026-01-10 16:50:34 +0800

    尝试修复暗色图标#2

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -7,15 +7,7 @@ import { getPostUrlBySlug } from "../utils/url-utils";
 
 export let tags: string[];
 export let categories: string[];
-export let sortedPosts: {
-	slug: string;
-	data: {
-		title: string;
-		tags: string[];
-		category?: string | null;
-		published: Date;
-	};
-}[] = [];
+export let sortedPosts: Post[] = [];
 
 const params = new URLSearchParams(window.location.search);
 tags = params.has("tag") ? params.getAll("tag") : [];
